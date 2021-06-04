ac3b0398683a1a3f62fa4dba095eb2950d4328b2
rady-ben <radhouanebenbrahim@gmail.com>
2021-06-04 19:45:07 +0100

    resfractor Question component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import {
-Paper, 
-Container
-} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Header from './component/Header';
 import Drawer from './component/Drawer';
-import { DRAWER_WIDTH } from './constant/ui';
+import Question from './component/Question';
+
 
 
 const modules = [
@@ -24,47 +21,13 @@ const modules = [
   'Module 12',
 ]
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-    marginLeft: theme.spacing(4) + DRAWER_WIDTH,
-    marginRight: theme.spacing(4),
-    padding: theme.spacing(2)
-  },
-}));
 
 function App() {
-  const classes = useStyles();
   return (
     <div>
       <Header />
       <Drawer listItems={modules} />
-
-      <Paper className={classes.container}>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        <p>
-          dddddddd
-        </p>
-        
-      </Paper>
+      <Question />
     </div>
   );
 }
---
+++ src/component/Question.js
@@ -0,0 +1,64 @@
+import {
+    Paper,
+    Typography,
+    Divider,
+    FormControlLabel,
+    Checkbox
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { DRAWER_WIDTH } from '../constant/ui';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        marginTop: theme.spacing(4),
+        marginBottom: theme.spacing(4),
+        marginLeft: theme.spacing(4) + DRAWER_WIDTH,
+        marginRight: theme.spacing(4),
+        padding: theme.spacing(2)
+    },
+    divider: {
+        marginBottom: theme.spacing(2),
+        marginTop: theme.spacing(2),
+    }
+
+}));
+
+const Question = () => {
+    const classes = useStyles();
+    return (
+        <Paper className={classes.container}>
+        <Typography 
+            variant="h4"
+        >
+            Question 1
+        </Typography>
+        <Divider className={classes.divider} />
+        <Typography variant="subtitle1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Divider className={classes.divider} />
+        <FormControlLabel
+              control={
+                <Checkbox checked={false} onChange={()=>{}} value="gilad" />
+              }
+              label="Gilad Gray"
+            />
+      </Paper>
+    );
+}
+
+export default Question;
