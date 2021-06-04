5a3c55c8ff4f93c667895b05c52c040f1ba9d6b6
rady-ben <radhouanebenbrahim@gmail.com>
2021-06-04 19:18:24 +0100

    add question compoenent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {
+Paper, 
+Container
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Header from './component/Header';
 import Drawer from './component/Drawer';
+import { DRAWER_WIDTH } from './constant/ui';
+
 
 const modules = [
   'Module 1',
@@ -17,12 +24,47 @@ const modules = [
   'Module 12',
 ]
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    marginLeft: theme.spacing(4) + DRAWER_WIDTH,
+    marginRight: theme.spacing(4),
+    padding: theme.spacing(2)
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div>
       <Header />
       <Drawer listItems={modules} />
+
+      <Paper className={classes.container}>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        <p>
+          dddddddd
+        </p>
+        
+      </Paper>
     </div>
   );
 }
