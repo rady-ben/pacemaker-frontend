e276a7cfffd3465abbc1c23d3cf7b12307b6b003
rady-ben <radhouanebenbrahim@gmail.com>
2021-08-21 14:34:43 +0100

    add comment section

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -143,6 +143,21 @@ const Question = () => {
                     ))
                 }
         </List>
+                {validated && <>
+                    <Divider className={classes.divider} />
+                    <Typography
+                        variant="h6"
+                    >
+                        Commentaire
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                    </Typography>
+                </>
+                }
         <Divider className={classes.divider} />
         <Box display="flex" justifyContent="space-between" m={1} p={1} bgcolor="background.paper">
             <Button 
