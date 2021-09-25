be20265119fd6d50ebf6b1acbe4c53b11906bca8
rady-ben <radhouanebenbrahim@gmail.com>
2021-09-25 16:20:50 +0100

    fix https

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const MODULES_API = 'http://serie200-api.herokuapp.com/v1.0/module/';
+export const MODULES_API = 'https://serie200-api.herokuapp.com/v1.0/module/';
 export const QUESTIONS_API = ({ 
     moduleId, courseId 
-}) =>`http://serie200-api.herokuapp.com/v1.0/module/${moduleId}/course/${courseId}/`;
+}) =>`https://serie200-api.herokuapp.com/v1.0/module/${moduleId}/course/${courseId}/`;
