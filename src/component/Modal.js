0df5a829c97347901e66717feed0d2734ae1d0be
rady-ben <radhouanebenbrahim@gmail.com>
2022-01-16 13:30:41 +0100

    fix modal ui

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,57 +1,28 @@
 import {
-    Typography,
     Dialog,
-    DialogTitle,
     DialogContent,
     DialogActions,
     Button,
 } from '@material-ui/core';
 import ReactMarkdown from 'react-markdown'
-import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
 import { CLOSE } from '../constant/text';
 
+const useStyles = makeStyles((theme) => ({
+    markdown: {
+        '& H1': {
+            fontSize: 18,
+        }
+    },
+}));
 
-const styles = (theme) => ({
-    root: {
-        margin: 0,
-        padding: theme.spacing(2),
-    },
-    titleText: {
-        maxWidth: '90%',
-    },
-    closeButton: {
-        position: 'absolute',
-        right: theme.spacing(1),
-        top: theme.spacing(1),
-        color: theme.palette.grey[500],
-    },
-});
-
-const CustomDialogTitle = withStyles(styles)((props) => {
-    const { children, classes, onClose } = props;
-    return (
-        <DialogTitle disableTypography className={classes.root}>
-            <Typography
-                variant="h6"
-                className={classes.titleText}
-            >
-                {children}
-            </Typography>
-            <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-                <CloseIcon />
-            </IconButton>
-        </DialogTitle>
-    );
-});
 
 const CustomModal = ({
     showSynthesis,
     toggleModal,
     modalContent,
 }) => {
-
+    const classes = useStyles();
     return (
         <Dialog
             open={showSynthesis}
@@ -60,7 +31,9 @@ const CustomModal = ({
             aria-describedby="alert-dialog-description"
         >
             <DialogContent>
-                <ReactMarkdown>{modalContent}</ReactMarkdown>
+                <div className={classes.markdown}>
+                    <ReactMarkdown>{modalContent}</ReactMarkdown>
+                </div>
             </DialogContent>
         <DialogActions>
           <Button onClick={toggleModal}>
