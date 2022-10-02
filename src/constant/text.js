845508011211b6f5140f2adbfece9ab0b66f4d99
radhouane-benbrahin <109966473+radhouane-benbrahin@users.noreply.github.com>
2022-10-02 13:40:47 +0100

    add some const messages

--- src/constant/text.js
+++ src/constant/text.js
@@ -1,14 +1,29 @@
-export const HOME = 'Accueil';
-export const SERIES_200 = 'Séries 200';
-export const QUESTION = 'Question';
-export const SYNTHESIS = 'Synthèse';
-export const CORRECT_ANSWER = 'Réponse juste!';
-export const WRONG_ANSWER = 'Réponse fausse!';
-export const COMMENT = 'Commentaire';
-export const PERSONAL_COMMENT_PLACEHOLDER= 'Vous pouvez ajoutez vos note ici ..';
-export const VALIDATE = 'Valider';
-export const NEXT = 'Suivant';
-export const PREVIOUS = 'Précédent';
-export const REDO = 'Refaire';
-export const CLOSE = 'Fermer';
+// welcome page
+export const PACEMAKER = "Pacemaker";
+export const QCM = "QCM";
+export const WELCOME_MESSAGE_1 = `Votre guide qui vous accompagnera durant vos révisions pour le
+résidanat, profitez des nombreuses sources fiables est
+enrichissantes pour vôtre succès`;
+export const START_REVIEW = "Commencer votre révision";
+export const OUR_SOURCES = "Nos sources";
+export const WELCOME_MESSAGE_2 = `Pacemaker vous propose les sources suivantes, et on est à jour
+avec les dernières source pour mieux préparer votre examen.`;
+export const MORE_INFO = `Pour plus d'informations veuillez nous suivre sur notre`;
+export const FACEBOOK_PAGE = "page facebook";
+
+// workspace
+export const HOME = "Accueil";
+export const SERIES_200 = "Séries 200";
+export const QUESTION = "Question";
+export const SYNTHESIS = "Synthèse";
+export const CORRECT_ANSWER = "Réponse juste!";
+export const WRONG_ANSWER = "Réponse fausse!";
+export const COMMENT = "Commentaire";
+export const PERSONAL_COMMENT_PLACEHOLDER =
+  "Vous pouvez ajoutez vos note ici ..";
+export const VALIDATE = "Valider";
+export const NEXT = "Suivant";
+export const PREVIOUS = "Précédent";
+export const REDO = "Refaire";
+export const CLOSE = "Fermer";
 export const ALERT_UNAVAILABLE_MODULE = `Ce module n'est pas encore disponible, il sera ajouté durant les prochains jours. Restez connecté :)`;
